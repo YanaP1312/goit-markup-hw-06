17339cdba3304a40327da60ab1d5712f9af806cd
Yana Pechenenko <pechenenko.ya@gmail.com>
2025-04-22 17:28:41 +0300

    add no-scroll and toast

--- common.js
+++ common.js
@@ -6,9 +6,11 @@ const closeModalBtn = document.querySelector(".form-close-btn");
 
 const openModal = () => {
   modalOverlay.classList.add("is-open");
+  document.body.classList.add("no-scroll");
 };
 const closeModal = () => {
   modalOverlay.classList.remove("is-open");
+  document.body.classList.remove("no-scroll");
   openModalBtn.blur();
 };
 
@@ -20,6 +22,33 @@ modalOverlay.addEventListener("click", (e) => {
   }
 });
 
+function showToast(message, duration = 3000) {
+  const toast = document.createElement("div");
+  toast.className = "toast";
+  toast.textContent = message;
+  document.body.appendChild(toast);
+
+  setTimeout(() => toast.classList.add("show"), 100);
+
+  setTimeout(() => {
+    toast.classList.remove("show");
+    setTimeout(() => toast.remove(), 400);
+  }, duration);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector(".review-form");
+
+  if (!form) return;
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    showToast("Thank you, our manager will contact you!");
+    closeModal();
+    form.reset();
+  });
+});
+
 // mob menu
 const closeMobMenuBtn = document.querySelector(".mobile-menu-close-btn");
 const mobMenu = document.querySelector(".mobile-menu");
@@ -28,9 +57,11 @@ const linkNav = document.querySelectorAll(".link");
 
 const openMobMenu = () => {
   mobMenu.classList.add("is-open");
+  document.body.classList.add("no-scroll");
 };
 const closeMobMenu = () => {
   mobMenu.classList.remove("is-open");
+  document.body.classList.remove("no-scroll");
   openMobMenuBtn.blur();
 };
 
